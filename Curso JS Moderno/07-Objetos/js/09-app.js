2403db1a5be6c6326203529f04ae2a5ed8437e68
Christian54r <cris38421@hotmail.com>
2025-12-09 14:28:57 -0600

    Cambios en app 10

---
+++ Curso JS Moderno/07-Objetos/js/09-app.js
@@ -0,0 +1,24 @@
+//Unir dos objetos
+//1 Objeto
+const producto = {
+    nombre: "Monitor 20 Pulgadas",
+    precio: 300,
+    disponible: true,
+}
+//2 Objeto
+const medidas = {
+    peso:"1kg",
+    medida: "1m"
+}
+
+console.log(producto)
+console.log(medidas)
+
+//El metodo object.assign agrupa dos objetos en uno solo
+const resultado = Object.assign(producto,medidas);
+
+//Spread Operatorr o Rest Operator
+const resultado2 = { ...producto, ...medidas }
+
+console.log(resultado);
+console.log(resultado2);
