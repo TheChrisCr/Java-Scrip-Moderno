a2042da3762b487799ad969780b35da2686d5ff9
Christian54r <cris38421@hotmail.com>
2026-01-01 12:15:44 -0600

    Cambios en app

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,13 @@
+const numero = [10,20,30];
+
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+
+console.log(numero);
+console.log(meses);
+
+
+
+//Un arreglo que contiene datos de todo tipo
+
+const deTodo = ["Hola", 10, true, "si", null, {nombre: 'Christian', trabajo: 'Programador'}];
+console.log(deTodo)
