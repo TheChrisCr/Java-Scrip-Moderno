6230c990f10553ab26d3d3d5d17d436626d6d2dd
Christian54r <cris38421@hotmail.com>
2026-01-02 18:11:39 -0600

    Cambios 05-apps

---
+++ Curso JS Moderno/08-Arrays/js/04-app.js
@@ -0,0 +1,5 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+//Formas de modificar un arreglo
+meses[0] = "Nuevo Mes";
+meses[7] = "Ultimo mes"
+console.table(meses);
