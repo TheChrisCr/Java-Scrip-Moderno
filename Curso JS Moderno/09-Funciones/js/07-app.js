fec498c8516781d65269c37c677ca16e4ec6d691
Christian54r <cris38421@hotmail.com>
2026-01-07 16:35:51 -0600

    Cambios en app-07 funciones

---
+++ Curso JS Moderno/09-Funciones/js/07-app.js
@@ -0,0 +1,17 @@
+inicarApp();
+
+function inicarApp() {
+    console.log("Iniciando app...");
+    segundaFuncion();
+}
+
+function segundaFuncion() {
+    console.log("Desde la segunda funcion");
+
+    usuarioAutenticado("Pablo");
+}
+
+function usuarioAutenticado (usuario) {
+    console.log("Autenticando Usuario... Espere...");
+    console.log(`Usuario autenticado exitosamente: ${usuario} `);
+}
