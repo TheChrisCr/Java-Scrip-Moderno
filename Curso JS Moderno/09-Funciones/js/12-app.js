49c2b73ef800851f2425116bd7300bfd40645c0f
Christian Rodriguez Alvarado <thechriscr@MacBook-Air-de-Christian.local>
2026-01-10 22:48:12 -0600

    Cambios en 12-apps funciones

---
+++ Curso JS Moderno/09-Funciones/js/12-app.js
@@ -0,0 +1,15 @@
+const carrito = [
+    { nombre: "Monitor 27 pulgadas", precio: 500 },
+    { nombre: "Tarjeta Video RTX", precio: 800 },
+    { nombre: "Airpods Pro", precio: 400 },
+    { nombre: "Wacht GT3", precio: 300 },
+    { nombre: "Tablet lenovo", precio: 100 },
+]
+
+
+    const nuevoArreglo = carrito.map( (producto) => `${producto.nombre} - Precio: ${producto.precio}` )
+    carrito.forEach( (producto) => `${producto.nombre} - Precio: ${producto.precio}` );
+
+
+    console.log(nuevoArreglo);
+    
