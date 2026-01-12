0c8b94d95710108bc7020cbd5ec121431de79e02
Christian Rodriguez <cris38421@hotmail.com>
2026-01-11 20:19:39 -0600

    Cambios en 02-app estructuras

---
+++ Curso JS Moderno/10-Estructuras/js/01-app.js
@@ -0,0 +1,8 @@
+const puntaje = 1000;
+//Compara si es igual ==
+if(puntaje == 1000) {
+    console.log('Si es igual');
+}
+else {
+    console.log('No es igual');
+}
---
+++ Curso JS Moderno/10-Estructuras/js/02-app.js
@@ -0,0 +1,19 @@
+const puntaje = 1000;
+const puntaje2 = "1000";
+
+console.log(typeof puntaje);
+console.log(typeof puntaje2);
+
+//compara si es diferente !=
+// if(puntaje != 100) {
+//     console.log('Si es diferente');
+// }
+
+if(puntaje === "1000") {
+    console.log('Si es igual');
+} else {
+    console.log('No es igual');
+}
+
+// == operador que no es estricto
+// === operador estricto
