ec423015b3a82b3a93c33bf51702611e596c3ea7
Christian Rodriguez <cris38421@hotmail.com>
2026-01-12 18:55:39 -0600

    Cambios en 03-apps funciones

---
+++ Curso JS Moderno/10-Estructuras/js/03-app.js
@@ -0,0 +1,19 @@
+//Operador mayor que y menor que
+
+const dinero = 300;
+const totalPagar = 300;
+
+//Mayor que
+if(dinero >= totalPagar){
+    console.log("Puedes pagar");
+} else {
+    console.log("Fondos insuficientes");
+}
+
+//Menor que
+// if(dinero <= totalPagar){
+//     console.log("Fondos insuficientes");
+// } else {
+//     console.log("Puedes pagar");
+// }
+
