ec1a6bbe00a71efcceb19382aee9db0e3480e915
Christian Rodriguez <cris38421@hotmail.com>
2026-01-15 18:12:07 -0600

    Cambios en 05-app iteradores

--- Curso JS Moderno/11-Iteradores/js/04-app.js
+++ Curso JS Moderno/11-Iteradores/js/04-app.js
@@ -2,6 +2,5 @@ let i = 10; //Iniciazar el codigo
 
 while(i < 10) { //Condicion
     console.log(`Numero ${i}`)
-
     i++; //Incremento
 }
---
+++ Curso JS Moderno/11-Iteradores/js/05-app.js
@@ -0,0 +1,8 @@
+let i = 1000; //Inicio
+
+
+//la diferencia de do while es que primero se ejecuta el do y luego se ejecuta el while que es la condicion
+do {
+    console.log(`Numero ${i}`)
+    i++ //Incremento
+} while ( i < 10); //condicion
