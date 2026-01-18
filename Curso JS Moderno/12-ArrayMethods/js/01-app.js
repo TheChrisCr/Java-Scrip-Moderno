f2d9683958b8e94009a0f2984f5a94d913387d19
Christian Rodriguez <cris38421@hotmail.com>
2026-01-18 16:33:17 -0600

    Cambios en 01-app ArraysMethods

--- Curso JS Moderno/12-ArrayMethods/js/01-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/01-app.js
@@ -8,3 +8,32 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+//Comprobar si un valor existen en un arreglo
+
+meses.forEach( mes => {
+    if (mes === `Enero`) {
+        console.log(`El mes de ${mes} si existe`)
+    }
+})
+
+
+const resultado1 = meses.includes(`Enero`);
+console.log(resultado1);
+
+const resultado2 = meses.includes(`Diciembre`);
+console.log(resultado2);
+
+//En un arreglo de objetos .some
+
+const existe = carrito.some( producto => producto.nombre === `Celular` )
+
+console.log(existe);
+
+const existe2 = carrito.some( producto => producto.nombre === `Monitor Curvo` )
+
+console.log(existe2);
+
+//En un arreglo  tradicional con .some
+const existe3 = meses.some( mes => mes === `Febrero` );
+console.log(existe3)
